4729d630838f364d290f737895f284aa48acbcbd
byrkam <66206274+byrkam@users.noreply.github.com>
2022-07-01 12:15:12 +0300

    Update Logging.py

--- Logging.py
+++ Logging.py
@@ -3,14 +3,13 @@ import subprocess
 
 
 class LogFiles:
-    def __init__(self, AP_sec):
+    def __init__(self):
         now = datetime.now()
-        self.AP_sec = AP_sec
         self.folder_name = datetime.now().strftime("fuzz_mngmt_frames")
         self.folder_path = 'Logs/' + self.folder_name
-        self.deauth_path = self.folder_path + datetime.now().strftime(f"/Deauth_path_{self.AP_sec}_%d-%m-%y__%H:%M:%S")
-        self.is_alive_path = self.folder_path + datetime.now().strftime(f"/Aliveness_check_{self.AP_sec}_%d-%m-%y__%H:%M:%S")
-        self.frames_till_disr = self.folder_path + datetime.now().strftime(f"/frames_till_disr_{self.AP_sec}_%d-%m-%y__%H:%M:%S")
+        self.deauth_path = self.folder_path + datetime.now().strftime(f"/Deauth_path_%d-%m-%y__%H:%M:%S")
+        self.is_alive_path = self.folder_path + datetime.now().strftime(f"/Aliveness_check_%d-%m-%y__%H:%M:%S")
+        self.frames_till_disr = self.folder_path + datetime.now().strftime(f"/frames_till_disr_%d-%m-%y__%H:%M:%S")
         subprocess.call(['mkdir -m 777 -p Logs'], shell = True)
         subprocess.call(['mkdir -m 777 -p ' + self.folder_path], shell = True)
 
