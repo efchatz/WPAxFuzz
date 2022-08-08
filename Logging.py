5a949396e7d8858dbddd2050bfc03b0998ac8c7e
byrkam <66206274+byrkam@users.noreply.github.com>
2022-08-08 19:08:09 +0300

    Update Logging.py

--- Logging.py
+++ Logging.py
@@ -5,13 +5,17 @@ import subprocess
 class LogFiles:
     def __init__(self):
         now = datetime.now()
-        self.folder_name = datetime.now().strftime("fuzz_mngmt_frames")
-        self.folder_path = 'Logs/' + self.folder_name
-        self.deauth_path = self.folder_path + datetime.now().strftime(f"/Deauth_path_%d-%m-%y__%H:%M:%S")
-        self.is_alive_path = self.folder_path + datetime.now().strftime(f"/Aliveness_check_%d-%m-%y__%H:%M:%S")
-        self.frames_till_disr = self.folder_path + datetime.now().strftime(f"/frames_till_disr_%d-%m-%y__%H:%M:%S")
+        self.folder_name_mngmt = datetime.now().strftime("fuzz_mngmt_frames")
+        self.folder_name_ctrl = datetime.now().strftime("fuzz_ctrl_frames")
+        self.folder_path_mngmt = 'Logs/' + self.folder_name_mngmt
+        self.folder_path_ctrl = 'Logs/' + self.folder_name_ctrl
+        self.deauth_path_mngmt = self.folder_path_mngmt + datetime.now().strftime(f"/Deauth_path_%d-%m-%y__%H:%M:%S")
+        self.is_alive_path_mngmt = self.folder_path_mngmt + datetime.now().strftime(f"/Aliveness_check_%d-%m-%y__%H:%M:%S")
+        self.is_alive_path_ctrl = self.folder_path_ctrl + datetime.now().strftime(f"/Aliveness_check_%d-%m-%y__%H:%M:%S")
+        self.frames_till_disr_mngmt = self.folder_path_mngmt + datetime.now().strftime(f"/frames_till_disr_%d-%m-%y__%H:%M:%S")
         subprocess.call(['mkdir -m 777 -p Logs'], shell = True)
-        subprocess.call(['mkdir -m 777 -p ' + self.folder_path], shell = True)
+        subprocess.call(['mkdir -m 777 -p ' + self.folder_path_mngmt], shell = True)
+        subprocess.call(['mkdir -m 777 -p ' + self.folder_path_ctrl], shell = True)
 
     def logging_conn_loss(self, reason, write_to):
         f = open(write_to, "a")
