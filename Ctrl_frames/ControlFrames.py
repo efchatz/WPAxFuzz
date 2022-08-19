4735c0ec0018b988d246b87180f20dd49fe8776c
byrkam <66206274+byrkam@users.noreply.github.com>
2022-08-19 14:31:40 +0300

    Update ControlFrames.py

--- Ctrl_frames/ControlFrames.py
+++ Ctrl_frames/ControlFrames.py
@@ -6,63 +6,206 @@ from time import sleep
 from random import randint
 import settings
 from Logging import LogFiles
-from generateBytes import *
+from generateBytes import generate_bytes
 import binascii
 
-NUM_OF_BATCHES = 4
-FRAME_NAMES = ['Beamforming Report Poll', 'VHT/HE NDP Announcement', 'Control Frame Extension', 'Control wrapper', 'Block Ack Request (BAR)', 'Block Ack', 'PS-Poll (Power Save-Poll)', 'RTS–Request to Send', 'CTS-Clear to Send', 'ACK', 'CF-End (Contention Free-End)', 'CF-End & CF-ACK']
+NUM_OF_FRAMES_TO_SEND = 64
 
 class ControlFrames:
 
-    def __init__(self, dest_addr, source_addr, interface):
+    def __init__(self, dest_addr, source_addr, interface, mode, frame_id, ctrl_frm_ext):
         self.dest_addr = dest_addr
         self.source_addr = source_addr
         self.interface = interface
-
-    def generate_MAC_header(self, frame_id, flag_byte):
-        dot11 = Dot11(type=1, subtype=frame_id, FCfield=flag_byte, addr1=self.dest_addr, addr2=self.source_addr)
+        self.mode = mode
+        self.frame_id = frame_id
+        self.ctrl_frm_ext = ctrl_frm_ext
+        self.ctrl_subtypes = [
+               {
+                "frame_id": 4,
+                "frame_name": "Beamforming Report Poll",
+                "payload_size": 1,
+                },
+               {
+                 "frame_id": 5,
+                "frame_name": "VHT/HE NDP Announcement",
+                "payload_size": 5,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 2,
+                "frame_name": "Poll",
+                "payload_size": 2,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 3,
+                "frame_name": "Service period request",
+                "payload_size": 7,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 4,
+                "frame_name": "Grant",
+                "payload_size": 7,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 5,
+                "frame_name": "DMG CTS",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 6,
+                "frame_name": "DMG DTS",
+                "payload_size": 6,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 7,
+                "frame_name": "Grant Ack",
+                "payload_size": 7,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 8,
+                "frame_name": "Sector sweep (SSW)",
+                "payload_size": 6,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 9,
+                "frame_name": "Sector sweep feedback (SSW-Feedback)",
+                "payload_size": 8,
+                },
+               {
+                "frame_id": 6,
+                "ctrl_ext_turn": 10,
+                "frame_name": "Sector sweep Ack (SSW-Ack)",
+                "payload_size": 8,
+                },
+               {
+                "frame_id": 7,
+                "frame_name": "Control wrapper",
+                "payload_size": 28,
+                },
+               {
+                "frame_id": 8,
+                "frame_name": "Block Ack Request (BAR)",
+                "payload_size": 16,
+                },
+               {
+                "frame_id": 9,
+                "frame_name": "Block Ack",
+                "payload_size": 16,
+                },
+               {
+                "frame_id": 10,
+                "frame_name": "PS-Poll (Power Save-Poll)",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 11,
+                "frame_name": "RTS–Request to Send",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 12,
+                "frame_name": "CTS-Clear to Send",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 13,
+                "frame_name": "ACK",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 14,
+                "frame_name": "CF-End (Contention Free-End)",
+                "payload_size": 0,
+                },
+               {
+                "frame_id": 15,
+                "frame_name": "CF-End & CF-ACK",
+                "payload_size": 0,
+                }
+        ]
+        self.fuzzer_state = {
+            "Frame Control Flags": {
+                "send_function": self.generate_frame_with_random_FCf,
+                "conn_loss": False
+                },
+            "payload": {
+                "send_function": self.generate_frame_with_random_payload,
+                "conn_loss": False
+                },
+            "Frame Control Flags + payload": {
+                "send_function": self.generate_frame_with_random_FCf_and_payload,
+                "conn_loss": False
+                },
+        }
+        
+    def extract_frame_info(self):
+        for frame_type in self.ctrl_subtypes:
+            if self.frame_id + 3 == frame_type["frame_id"]:
+                if frame_type["frame_id"] == 6:
+                    if frame_type["ctrl_ext_turn"] == self.ctrl_frm_ext:
+                        return frame_type
+                else:
+                    return frame_type
+        
+    def construct_bytes(self, num_of_bytes):
+        payload = bytearray(b'')
+        for item in generate_bytes(num_of_bytes, self.mode):
+            payload.append(item)
+        return bytes(payload)
+        
+    def generate_MAC_header(self, FCf=0):
+        dot11 = Dot11(type=1, subtype=self.frame_id+3, FCfield=FCf, addr1=self.dest_addr, addr2=self.source_addr)
         MAC_header = RadioTap() / dot11
         return MAC_header
         
-    def generate_random_payload(self, frame_id, flag_byte, multiplier):
-        MAC_header = self.generate_MAC_header(frame_id, flag_byte)
+    def generate_random_payload(self):
+        frame_info = self.extract_frame_info()
+        if self.mode == 'standard':
+            if frame_info['payload_size'] != 0:
+                if self.frame_id + 3 in {4,5,6}:
+                    if self.frame_id + 3 == 6:
+                        if self.ctrl_frm_ext == 5:
+                            return binascii.unhexlify(self.source_addr.replace(':', ''))
+                        else:
+                            return binascii.unhexlify(self.source_addr.replace(':', '')) + self.construct_bytes(frame_info['payload_size'])
+                    else:
+                        return binascii.unhexlify(self.source_addr.replace(':', '')) + self.construct_bytes(frame_info['payload_size'])
+                else:
+                    return self.construct_bytes(frame_info['payload_size'])
+            else:
+                if self.frame_id + 3 in {4,5,6}:
+                    return binascii.unhexlify(self.source_addr.replace(':', ''))
+                else:
+                    return b''
+        elif self.mode == 'random':
+            if self.frame_id + 3 in {4,5,6}:
+                return binascii.unhexlify(self.source_addr.replace(':', '')) + self.construct_bytes(frame_info['payload_size'])
+            else:
+                return self.construct_bytes(frame_info['payload_size'])
+
+    def generate_frame_with_random_FCf(self):
+        return self.generate_MAC_header(int.from_bytes(self.construct_bytes(1), "big"))
         
-        def payload_portion(num_of_bytes):
-            payload = bytearray(b'')
-            for item in generate_bytes(num_of_bytes, 'standard'):
-                payload.append(item)
-            return bytes(payload)
+    def generate_frame_with_random_payload(self, FCf=0):
+        MAC_header = self.generate_MAC_header(FCf)
+        payload = self.generate_random_payload()
+        return MAC_header / payload
         
-        
-        if multiplier == 1:
-            if frame_id in {4,5,6}:
-                return MAC_header / binascii.unhexlify(self.source_addr.replace(':', '')) / payload_portion(26)
-            else:
-                return MAC_header / payload_portion(32)
-        elif multiplier == 2:
-            if frame_id in {4,5,6}:
-                return MAC_header / binascii.unhexlify(self.source_addr.replace(':', '')) / payload_portion(26) / payload_portion(32)
-            else:
-                return MAC_header / payload_portion(32) / payload_portion(32)
-        elif multiplier == 3:
-            if frame_id in {4,5,6}:
-                return MAC_header / binascii.unhexlify(self.source_addr.replace(':', '')) / payload_portion(26) / payload_portion(32) / payload_portion(32) / payload_portion(32)
-            else:
-                return MAC_header / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32)
-        elif multiplier == 4:
-            if frame_id in {4,5,6}:
-                return MAC_header / binascii.unhexlify(self.source_addr.replace(':', '')) / payload_portion(26) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32)
-            else:
-                return MAC_header / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32) / payload_portion(32)
             
-    def print_message(self, fuzz_stage):
-        subprocess.call(['clear'], shell=True)
-        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
-        subprocess.call(['echo' + f' Fuzzing with Control Frames: Testing {fuzz_stage}\n'], shell=True)
-        subprocess.call(['echo' + f' {bcolors.OKGREEN}Fuzzing process will exit automatically{bcolors.ENDC}\n'], shell=True)
-        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n')
-        
-    def check_conn_aliveness(self, frame):
+    def generate_frame_with_random_FCf_and_payload(self):
+        MAC_header = self.generate_MAC_header(int.from_bytes(self.construct_bytes(1), "big"))
+        payload = self.generate_random_payload()
+        return MAC_header / payload
+      
+    def check_conn_aliveness(self, frame, fuzzing_stage):
         
         def check_conn():
             sleep(2)
@@ -71,11 +214,13 @@ class ControlFrames:
             return
         
         if not settings.is_alive:
+            self.fuzzer_state[fuzzing_stage]["conn_loss"] = True
             print('\nHexDump of frame:')
             hexdump(frame)
             check_conn()
             return True
         elif settings.conn_loss:
+            self.fuzzer_state[fuzzing_stage]["conn_loss"] = True
             print('\nHexDump of frame:')
             hexdump(frame)
             input(f'\n{bcolors.FAIL}Deauth or Disass frame found.{bcolors.ENDC}\n\n{bcolors.WARNING}Reconnect, if needed, and press Enter to resume:{bcolors.ENDC}\n')
@@ -88,49 +233,56 @@ class ControlFrames:
         return False
         
     def fuzz_ctrl_frames(self):
+        counter = 1
         init_logs = LogFiles()
-        self.print_message(f" {bcolors.OKBLUE}Frame Control Flags{bcolors.ENDC}")    
-        for frame_id in range(4, 16):
-            print(f'Sending {bcolors.OKBLUE}{FRAME_NAMES[frame_id-16]}{bcolors.ENDC} frames with Frame Control Flags checked within the range (0,256)')
-            for flag_byte in range (0, 256):
-                frame = self.generate_MAC_header(frame_id, flag_byte)
-                if self.check_conn_aliveness(frame):
-                    init_logs.logging_conn_loss(f"Connectivity issues detected while sending {FRAME_NAMES[frame_id-16]} frames with Frame Control Flags equal to {flag_byte}\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
-                    break
-                else:
-                    sendp(frame, count=128, iface=self.interface, verbose=0)
-                if frame_id == 6 and flag_byte == 11:
-                     break
-        subprocess.call(['clear'], shell=True)       
-        self.print_message(f" {bcolors.OKBLUE}Payload overflow{bcolors.ENDC}")
-        for frame_id in range(4, 16):
-            print(f'Sending {bcolors.OKBLUE}{FRAME_NAMES[frame_id-16]}{bcolors.ENDC} frames with random payload multiplied in range (1,4)')
-            for multiplier in range(1, 5):
-                for _ in range(0, NUM_OF_BATCHES):
-                    frame = self.generate_random_payload(frame_id, randint(0,257), multiplier)
-                    if self.check_conn_aliveness(frame):
-                        init_logs.logging_conn_loss(f"Connectivity issues detected while sending {FRAME_NAMES[frame_id-16]} frames with payload size equal to {multiplier*32} bytes\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
-                        break
-                    else:
-                        sendp(frame, count=128, iface=self.interface, verbose=0)
-                else: #for-else to break nested loop
-                    continue
-                break
-        subprocess.call(['clear'], shell=True)
-        self.print_message(f" {bcolors.OKBLUE}Frame Control Flags + Payload overflow{bcolors.ENDC}")
-        for frame_id in range(4, 16):
-            print(f'Sending {bcolors.OKBLUE}{FRAME_NAMES[frame_id-16]}{bcolors.ENDC} frames with Frame Control Flags checked within the range (0,256) and with random payload multiplied in range (1,4)')
-            for flag_byte in range (0, 256):
-                for multiplier in range(1, 5):
-                    frame = self.generate_random_payload(frame_id, flag_byte, multiplier)
-                    if self.check_conn_aliveness(frame):
-                        init_logs.logging_conn_loss(f"Connectivity issues detected while sending {FRAME_NAMES[frame_id-16]} frames with Frame Control Flags equal to {flag_byte} and payload size equal to {multiplier*32} bytes\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
-                        break
-                    else:
-                        sendp(frame, count=128, iface=self.interface, verbose=0)
-                    if frame_id == 6 and flag_byte == 11:
-                        break
-                else: #for-else to break nested loop
-                    continue
-                break
-        print(f'{bcolors.FAIL}Exiting Fuzzer!!{bcolors.ENDC}')
+        if self.mode == 'standard' or self.mode == 'random':
+            subprocess.call(['clear'], shell=True)
+            print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+            print('You selected mode:', self.mode)
+            while True:
+                subprocess.call(['echo' + f' Fuzzing cycle No.{counter}\n'], shell=True)
+                subprocess.call(['echo' + f' {bcolors.OKGREEN}Stop the fuzzing and monitoring processes with 2 consecutive Ctrl+c{bcolors.ENDC}\n'], shell=True)
+                print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n')
+                frame_info = self.extract_frame_info()
+                for i in self.fuzzer_state:
+                    if self.fuzzer_state[i]["conn_loss"] == True:
+                        continue
+                    if frame_info["frame_id"] == 6:
+                        subprocess.call(['clear'], shell=True)
+                        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+                        print('You selected mode:', self.mode)
+                        subprocess.call(['echo' + f' {bcolors.OKGREEN}Stop the fuzzing and monitoring processes with 2 consecutive Ctrl+c{bcolors.ENDC}\n'], shell=True)
+                        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n')
+                        print(f'Transmitting {bcolors.OKBLUE}{frame_info["frame_name"]}{bcolors.ENDC} frames with random payload')
+                        while True:
+                            frame =  self.fuzzer_state["payload"]["send_function"](self.ctrl_frm_ext)
+                            if(self.check_conn_aliveness(frame, i)):
+                                init_logs.logging_conn_loss(f"Connectivity issues detected while sending {frame_info['frame_name']} frames with random {i}\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
+                                break
+                            else:
+                                sendp(frame, count=2, iface=self.interface, verbose=0)
+                    elif frame_info["payload_size"] == 0 and self.mode == 'standard':
+                        subprocess.call(['clear'], shell=True)
+                        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+                        print('You selected mode:', self.mode)
+                        subprocess.call(['echo' + f' {bcolors.OKGREEN}Stop the fuzzing and monitoring processes with 2 consecutive Ctrl+c{bcolors.ENDC}\n'], shell=True)
+                        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n')
+                        print(f'Transmitting {bcolors.OKBLUE}{frame_info["frame_name"]}{bcolors.ENDC} frames with random Frame Control Flags')
+                        while True:
+                            frame =  self.fuzzer_state["Frame Control Flags"]["send_function"]()
+                            if(self.check_conn_aliveness(frame, i)):
+                                init_logs.logging_conn_loss(f"Connectivity issues detected while sending {frame_info['frame_name']} frames with random {i}\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
+                                break
+                            else:
+                                sendp(frame, count=2, iface=self.interface, verbose=0)
+                    else:   
+                        print(f'Transmitting {bcolors.OKBLUE}{frame_info["frame_name"]}{bcolors.ENDC} frames with random {i}')
+                        for _ in range(1, NUM_OF_FRAMES_TO_SEND):
+                            frame =  self.fuzzer_state[i]["send_function"]()
+                            if(self.check_conn_aliveness(frame, i)):
+                                init_logs.logging_conn_loss(f"Connectivity issues detected while sending {frame_info['frame_name']} frames with random {i}\nframe = {frame}\n\n", init_logs.is_alive_path_ctrl)
+                                break
+                            else:
+                                sendp(frame, count=2, iface=self.interface, verbose=0)
+                subprocess.call(['clear'], shell=True)
+                counter += 1  
