2f760e332ad7ec381cdaef080601722c69464e61
giannis dimtsas <giannisdimtsas@gmail.com>
2024-12-04 13:20:31 +0200

    Created new validations for command line arguments, renamed functions and fixed some typos.

--- fuzz.py
+++ fuzz.py
@@ -13,17 +13,22 @@ from src import utils
 
 parser = argparse.ArgumentParser(description="WPAxFuzz tool options", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-u", "--url", help="HTTP Server url. Cannot used combined with -a.")
-parser.add_argument("-p", "--port", type=int, help="Port. Cannot used combined with -a.")
+parser.add_argument("-u", "--url", help="HTTP Server url. Cannot used along with -a.")
+parser.add_argument("-p", "--port", type=int, help="Port. Cannot used along with -a.")
 parser.add_argument("-t", "--type", type=str, help="Specify the frame type to fuzz. Types are: 'management', 'control' and 'data'.")
 parser.add_argument("-s", "--subtype", type=int, help="Specify the frame Subtype.")
 parser.add_argument("-d", "--dos", action="store_true", help="DoS attack module.")
-parser.add_argument("-g", "--generator", type=str, choices=["blab", "gramfuzz"], help="Specify generator. Allowed generators: 'blab' and 'gramfuzz'.")
-parser.add_argument("-m", "--mode", type=str, choices=["standard", "random"], help="Specify mode option. Allowed options: 'standard' or 'random'.")
-parser.add_argument("-a", "--aliveness", type=str, choices=["yes", "no"], help="Specify if Aliveness will be set or not. Allowed options: 'yes' or 'no'. Cannot used combined with -u and -p")
+parser.add_argument("-g", "--generator", type=str, help="Specify generator. Allowed generators: 'blab' and 'gramfuzz'.")
+parser.add_argument("-m", "--mode", type=str, help="Specify mode option. Allowed options: 'standard' or 'random'.")
+parser.add_argument("-a", "--aliveness", type=str, help="Specify if Aliveness will be set or not. Allowed options: 'yes' or 'no'. Cannot used along with -u and -p")
 args = parser.parse_args()
 
-utils.validate_arguments(args.url, args.port, args.aliveness, args.dos, args.type, args.subtype, sys.argv)
+if len(sys.argv) == 1:
+    print("\n")
+    parser.print_help()
+    sys.exit(1)
+
+utils.argumentsValidation(args.url, args.port, args.aliveness, args.dos, args.type, args.subtype, args.generator, args.mode, sys.argv)
 
 if args.dos:
     subprocess.call(['sudo python3 mage.py'], shell=True)
--- src/utils.py
+++ src/utils.py
@@ -5,28 +5,21 @@ import subprocess
 
 from Msgs_colors import bcolors
 
-def validate_arguments(ip, port, aliveness, dos, type, subtype, arguments):
+def argumentsValidation(ip, port, aliveness, dos, type, subtype, generator, mode, arguments):
     frames = json.load(open('src/frames.json','r'))
     lowercase_frames = {key.lower(): value for key, value in frames.items()}
 
-    validate_frame(lowercase_frames, type, subtype)
-
-    if (ip and not port) or (not ip and port) or (not ip and not port and not aliveness):
-        print(bcolors.FAIL + "\n\t\tMonitoring method is not set.\n\t\tProvide a URL (-u) and a Port (-p) for HTTP Server or set Aliveness (-a) as a monitoring method.\n\t\tIf you don't want to set a Monitoring method, set Aliveness to 'no' (-a no)." + bcolors.ENDC)
-        os._exit(0)
-    elif not ip and not port:
-        return True
-    elif ip and port and aliveness:
-        print(bcolors.FAIL + "\n\t\tHTTP server is set as a monitoring method. Cannot set aliveness!" + bcolors.ENDC)
-        os._exit(0)
-    elif dos and len(arguments) > 2:
+    if dos and len(arguments) > 2:
         print(bcolors.FAIL + "\n\t\tCannot provide other arguments when -d (--dos) argument is set." + bcolors.ENDC)
         os._exit(0)
-    else:
-        validate_ip(ip)
-        validate_port(port)
 
-def validate_ip(ip):
+    frameValidation(lowercase_frames, type, subtype)
+    monitoringValidation(ip, port, aliveness, dos, arguments)
+    generatorValidator(generator)
+    modeValidator(mode)
+    alivnessValidator(aliveness)
+
+def ipValidation(ip):
     try:
         ipaddress.ip_address(ip)
         return True
@@ -34,13 +27,13 @@ def validate_ip(ip):
         print(bcolors.FAIL + f"\n\t\tThe IP {ip} is not valid!" + bcolors.ENDC)
         os._exit(0)
 
-def validate_port(port):
+def portValidation(port):
     if 0 < port <= 65535:
         return True
     print(bcolors.FAIL + "\n\t\tPort argument is not valid!" + bcolors.ENDC)
     os._exit(0)
     
-def validate_frame(frames, type, subtype):
+def frameValidation(frames, type, subtype):
 
     if type not in frames.keys():
         print(bcolors.FAIL + f"\n\t\tType is not valid!\n\t\tValid Types:'management', 'control', 'data'"+ bcolors.ENDC)
@@ -50,6 +43,36 @@ def validate_frame(frames, type, subtype):
         print(bcolors.FAIL + f"\n\t\tThis Subtype is not included in {type}'s subtypes!"+ bcolors.ENDC)
         os._exit(0)
 
+def monitoringValidation(ip, port, aliveness, dos, arguments):
+    if (ip and not port) or (not ip and port) or (not ip and not port and not aliveness):
+        print(bcolors.FAIL + "\n\t\tMonitoring method is not set.\n\t\tProvide a URL (-u) and a Port (-p) for HTTP Server or set Aliveness (-a) as a monitoring method.\n\t\tIf you don't want to set a Monitoring method, set Aliveness to 'no' (-a no)." + bcolors.ENDC)
+        os._exit(0)
+    elif not ip and not port:
+        return True
+    elif ip and port and aliveness:
+        print(bcolors.FAIL + "\n\t\tHTTP server is set as a monitoring method. Cannot set aliveness!" + bcolors.ENDC)
+        os._exit(0)
+    else:
+        ipValidation(ip)
+        portValidation(port)
+
+def generatorValidator(generator):
+    generators = ['blab', 'gramfuzz']
+    if generator not in generators:
+        print(bcolors.FAIL + f"\n\t\tThis is not a valid generator!\n\t\tValid generators are: 'blab' and 'gramfuzz'." + bcolors.ENDC)
+        os._exit(0)
+
+def modeValidator(mode):
+    modes = ['standard', 'random']
+    if mode not in modes:
+        print(bcolors.FAIL + f"\n\t\tThis is not a valid mode!\n\t\tValid generators are: 'standard' and 'random'." + bcolors.ENDC)
+        os._exit(0)
+
+def alivnessValidator(aliveness_option):
+    options = ['yes', 'no']
+    if aliveness_option not in options:
+        print(bcolors.FAIL + f"\n\t\tThis is not a valid aliveness option!\n\t\tValid aliveness options are: 'yes' and 'no'." + bcolors.ENDC)
+        os._exit(0)
 
 def start_sae(targeted_AP, AP_CHANNEL, AP_MAC_DIFFERENT_FREQUENCY, CHANNEL_DIFFERENT_FREQUENCY, targeted_STA, att_interface, MONITORING_INTERFACE, PASSWORD):
     terminal_width = int(subprocess.check_output(['stty', 'size']).split()[1])
